3032754804039ee0df0970ede70af4bd26b29113
Your Name <you@example.com>
2025-04-28 21:23:50 -0400

    Update routing to HashRouter for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom'; // Use HashRouter instead
+import { HashRouter } from 'react-router-dom'; // Import HashRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <HashRouter> {/* Use HashRouter instead of BrowserRouter */}
+    <HashRouter> {/* Wrap the App component with HashRouter */}
       <App />
     </HashRouter>
   </React.StrictMode>
