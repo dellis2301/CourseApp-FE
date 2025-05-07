bec8372227a53f88839765814309e79ca11047ab
dellis2301 <dellis75@ivytech.edu>
2025-05-07 19:46:21 -0400

    Update ProtectedRoute.js

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
-import { isAuthenticated, getUserRole } from './authUtils'; // Assuming you have an authUtils file for utility functions
+import { Navigate } from 'react-router-dom';
+import { isAuthenticated, getUserRole } from './authUtils';
 
-const ProtectedRoute = ({ element, requiredRole, ...rest }) => {
-  const userRole = getUserRole(); // Get the role of the authenticated user
+const ProtectedRoute = ({ element, requiredRole }) => {
+  const userRole = getUserRole();
 
-  // If the user is not authenticated, redirect to login
   if (!isAuthenticated()) {
     return <Navigate to="/login" />;
   }
 
-  // If the user doesn't have the required role (e.g., 'teacher'), redirect to home
   if (requiredRole && userRole !== requiredRole) {
-    return <Navigate to="/" />; // Or redirect to an error page, depending on your logic
+    return <Navigate to="/" />;
   }
 
-  // If the user is authenticated and has the correct role, render the requested component
-  return <Route {...rest} element={element} />;
+  return element;
 };
 
 export default ProtectedRoute;
